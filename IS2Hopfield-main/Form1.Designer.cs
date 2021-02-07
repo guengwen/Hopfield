07c0e8c823b9fe3189bc3052e8d3f7c7dc9fb039
guengwen <71259054+guengwen@users.noreply.github.com>
2021-02-07 22:28:59 +0800

    IS2- Updated UI
    Cariquitan

---
+++ IS2Hopfield-main/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace IS2HopfieldNetwork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.imageMagnifier1 = new ImageMagnifier.ImageMagnifier();
+            this.imageMagnifier2 = new ImageMagnifier.ImageMagnifier();
+            this.imageMagnifier3 = new ImageMagnifier.ImageMagnifier();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // imageMagnifier1
+            // 
+            this.imageMagnifier1.BackColor = System.Drawing.Color.LightCoral;
+            this.imageMagnifier1.ImageToMagnify = null;
+            this.imageMagnifier1.Location = new System.Drawing.Point(251, 125);
+            this.imageMagnifier1.MagnificationCoefficient = 1;
+            this.imageMagnifier1.Name = "imageMagnifier1";
+            this.imageMagnifier1.Size = new System.Drawing.Size(104, 96);
+            this.imageMagnifier1.TabIndex = 4;
+            this.imageMagnifier1.Text = "imageMagnifier1";
+            this.imageMagnifier1.Click += new System.EventHandler(this.imageMagnifier1_Click);
+            // 
+            // imageMagnifier2
+            // 
+            this.imageMagnifier2.BackColor = System.Drawing.Color.LightSalmon;
+            this.imageMagnifier2.ImageToMagnify = null;
+            this.imageMagnifier2.Location = new System.Drawing.Point(251, 226);
+            this.imageMagnifier2.MagnificationCoefficient = 1;
+            this.imageMagnifier2.Name = "imageMagnifier2";
+            this.imageMagnifier2.Size = new System.Drawing.Size(104, 96);
+            this.imageMagnifier2.TabIndex = 5;
+            this.imageMagnifier2.Text = "imageMagnifier2";
+            // 
+            // imageMagnifier3
+            // 
+            this.imageMagnifier3.BackColor = System.Drawing.Color.NavajoWhite;
+            this.imageMagnifier3.ImageToMagnify = null;
+            this.imageMagnifier3.Location = new System.Drawing.Point(251, 328);
+            this.imageMagnifier3.MagnificationCoefficient = 1;
+            this.imageMagnifier3.Name = "imageMagnifier3";
+            this.imageMagnifier3.Size = new System.Drawing.Size(104, 96);
+            this.imageMagnifier3.TabIndex = 7;
+            this.imageMagnifier3.Text = "imageMagnifier3";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(17, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Pattern";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(17, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 25);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Run Dynamics";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "0";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Tomato;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(65, 182);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(104, 96);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MistyRose;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(12, 125);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(231, 300);
+            this.panel2.TabIndex = 13;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Khaki;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(12, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(343, 107);
+            this.panel3.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Stylus BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(134, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 16);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "TEAM HOP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(92, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 23);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "HOPFIELD MODEL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(70, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 23);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "OF NEURAL NETWORK";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(365, 435);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.imageMagnifier3);
+            this.Controls.Add(this.imageMagnifier2);
+            this.Controls.Add(this.imageMagnifier1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private ImageMagnifier.ImageMagnifier imageMagnifier1;
+        private ImageMagnifier.ImageMagnifier imageMagnifier2;
+        private ImageMagnifier.ImageMagnifier imageMagnifier3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ IS2Hopfield-main/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using HopfieldNeuralNetwork;
+
+namespace IS2HopfieldNetwork
+{
+    public partial class Form1 : Form
+    {
+        NeuralNetwork NN = new NeuralNetwork(100);
+        Bitmap[] m = new Bitmap[10];
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = (NN.M).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //openFileDialog1.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                m[0] = new Bitmap(openFileDialog1.FileName);
+
+                List<Neuron> pattern = bitmapToPattern(m[0]);
+
+                imageMagnifier1.ImageToMagnify = m[0];
+                imageMagnifier1.MagnificationCoefficient = 10;
+
+                NN.AddPattern(pattern);
+                for (int i = 0; i < NN.Neurons.Count; i++)
+                {
+                    Console.WriteLine(NN.Neurons[i].State);
+                }
+
+                Bitmap bitmap = patternToBitmap(NN.Neurons);
+                //imageMagnifier3.ImageToMagnify = bitmap;
+                //imageMagnifier3.MagnificationCoefficient = 10;
+
+                List<Neuron> randomPattern = generateRandomPattern(100);
+                imageMagnifier2.ImageToMagnify = patternToBitmap(randomPattern);
+                imageMagnifier2.MagnificationCoefficient = 10;
+
+                label1.Text = (NN.M).ToString();
+            }
+        }
+
+        private List<Neuron> bitmapToPattern(Bitmap b)
+        {
+            int bitmapSize = b.Width * b.Height;
+            List<Neuron> p = new List<Neuron>(bitmapSize);
+            Color c;
+
+            for (int i = 0; i < b.Width; i ++)
+            {
+                for (int j = 0; j < b.Height; j++)
+                {
+                    Neuron n = new Neuron();
+
+                    c = b.GetPixel(i, j);
+
+                    n.State = c.R > 127 ? 1 : -1;
+
+                    p.Add(n);
+                    //Console.WriteLine(p[p.Count-1].State);
+                }
+            }
+
+            return p;
+        }
+
+        private Bitmap patternToBitmap(List<Neuron> p)
+        {
+            int size = 10;
+            Bitmap bitmap = new Bitmap(size, size);
+            Color c;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    int val = p[(size * i) + j].State > 0 ? 255 : 0;
+                    //Console.WriteLine(val + ": " + p[init.Width * i + j].State);
+
+                    c = Color.FromArgb(val, val, val);
+
+                    bitmap.SetPixel(i, j, c);
+                }
+            }
+
+            return bitmap;
+        }
+
+        private List<Neuron> generateRandomPattern(int size)
+        {
+            List<Neuron> pattern = new List<Neuron>();
+            Random rand  = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                Neuron n = new Neuron();
+                n.State = rand.Next(2) > 0 ? 1 : -1;
+                pattern.Add(n);
+            }
+
+            return pattern;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap b = new Bitmap(imageMagnifier2.ImageToMagnify);
+            List<Neuron> pattern = bitmapToPattern(b);
+            NN.Run(pattern);
+
+
+            imageMagnifier3.ImageToMagnify = patternToBitmap(NN.Neurons);
+            imageMagnifier3.MagnificationCoefficient = 10;            
+        }
+
+        private void imageMagnifier1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
